6bc7309b9f69524de18c64aaa5acc9a481d42e15
tim95bell <tim95bell@gmail.com>
2025-03-06 17:48:13 +0800

    update to using pks12 for ssl certificate, add script to generate self signed ssl certificate

---
+++ scripts/gen_ssl_certificate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd $(dirname $0)/../src/main/resources
+mkdir -p local
+pushd local
+
+set -e
+
+. ./local.env
+
+openssl req -newkey rsa:2048 -x509 -keyout key.pem -out cert.pem -days 365 --passout pass:$TIM95BELL_TODO_SSL_KEY_PASSWORD -subj /C=AU/O=tim95bell/
+
+openssl pkcs12 -export -in cert.pem -inkey key.pem -out certificate.p12 -name "certificate" -passin pass:$TIM95BELL_TODO_SSL_KEY_PASSWORD -passout pass:$TIM95BELL_TODO_SSL_CERTIFICATE_PASSWORD
+
+popd
+popd
