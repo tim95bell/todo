c352c88d1aab90ee12145e940177ba4d872d90e3
tim95bell <tim95bell@gmail.com>
2025-03-10 10:39:47 +0800

    start building todo_as (authentication server)

---
+++ todo_as/src/main/java/com/tim95bell/todo_as/TodoAsApplication.java
@@ -0,0 +1,13 @@
+package com.tim95bell.todo_as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAsApplication.class, args);
+	}
+
+}
---
+++ todo_as/src/test/java/com/tim95bell/todo_as/TodoAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim95bell.todo_as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
